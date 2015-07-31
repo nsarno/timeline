087f5053bf4b16bb17094b00a0875e2ade0c4d00
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2015-07-31 11:38:26 +0200

    Add some tests to Timeline component + improve shallowRender function

--- test/components/Timeline-test.js
+++ test/components/Timeline-test.js
@@ -2,9 +2,32 @@ var test = require('tape');
 var shallowRender = require('../support/shallowRender');
 var Timeline = require('../../app/scripts/components/Timeline');
 
-const component = shallowRender(Timeline);
+var thread = [
+  { label: "Move to Australia", emoji: "🐨" },
+  { label: "Release Knock", emoji: "💎", card: {
+      img: "images/knock.png",
+      header: "Release Knock on RubyGems",
+      content: "Knock is an authentication solution for Rails API-only applications based on JSON Web Tokens."
+    }
+  },
+  { date: "2015" }
+];
+
+const component = shallowRender(Timeline, {thread: thread});
 
 test('Timeline component has a valid type', function (t) {
   t.plan(1);
   t.equal(component.type, 'div');
 });
+
+test('Timeline component has a valid class', function (t) {
+  t.plan(1);
+  t.equal(component.props.className, 'tl');
+});
+
+test('Timeline component has a thread child', function (t) {
+  t.plan(1);
+  t.equal(component.props.children.type.displayName, 'Thread');
+});
+
+console.log(component);
--- test/support/shallowRender.js
+++ test/support/shallowRender.js
@@ -2,8 +2,8 @@ var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 var shallowRenderer = TestUtils.createRenderer();
 
-var shallowRender = function (component) {
-  shallowRenderer.render(React.createElement(component));
+var shallowRender = function (component, props={}, ...children) {
+  shallowRenderer.render(React.createElement(component, props, children));
   return shallowRenderer.getRenderOutput();
 };
 
